af711571020383b1f676e6a84309a75f26727339
Akshat Agrawal <91063455+akshatagr02@users.noreply.github.com>
2025-10-24 13:34:55 +0530

    24-10-25
    firebase config

--- app/utils/firebaseConfig.js
+++ app/utils/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3dUqSDit3Fj6LaYEa-yP11Zkh_KzYQHE",
-  authDomain: "librarymanager-c1e8c.firebaseapp.com",
-  databaseURL: "https://librarymanager-c1e8c-default-rtdb.firebaseio.com",
-  projectId: "librarymanager-c1e8c",
-  storageBucket: "librarymanager-c1e8c.firebasestorage.app",
-  messagingSenderId: "671641811811",
-  appId: "1:671641811811:web:3c371abf9643783830b156",
-  measurementId: "G-801H3KD4K8"
+  apiKey: "APIK_EY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "MEASUREMENT_ID"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
